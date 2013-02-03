4b168dc81042429c1b4e833b9dce63e04acfd146
Tiago Cunha <tcunha@gmx.com>
2013-02-03 20:26:57 +0000

    Add myself to the copyright.

--- ecmd_rmfile.c
+++ ecmd_rmfile.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2010, 2011, 2012 Ryan Flannery <ryan.flannery@gmail.com>
+ * Copyright (c) 2013 Tiago Cunha <tcunha@gmx.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
